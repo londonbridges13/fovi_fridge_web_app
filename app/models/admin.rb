9f446e77b8626c431df217782fd6c8feaac87aa6
Lyndon McKay <lyndonmckay13@gmail.com>
2016-07-12 09:41:46 -0500

    removed registratable

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -2,6 +2,6 @@ class Admin < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable,
-         :recoverable, :rememberable, :trackable, :validatable, :lockable, :registerable
+         :recoverable, :rememberable, :trackable, :validatable, :lockable#, :registerable
          # removed :registerable so hacker cant create new Admin
 end
