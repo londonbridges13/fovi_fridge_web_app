e1cdf0bb922ea81857b2aedfda1261158a6090a4
Lyndon McKay <lyndonmckay13@gmail.com>
2016-07-12 02:41:54 -0500

    Removed Rack-Attack blocklist

--- config/initializers/rack-attack.rb
+++ config/initializers/rack-attack.rb
@@ -64,29 +64,29 @@ class Rack::Attack
     end
   end
 
-
-
-  # Lockout IP addresses that are hammering your login page.
-  # After 20 requests in 1 minute, block all requests from that IP for 1 hour.
-  Rack::Attack.blocklist('allow2ban login scrapers') do |req|
-    # `filter` returns false value if request is to your login page (but still
-    # increments the count) so request below the limit are not blocked until
-    # they hit the limit.  At that point, filter will return true and block.
-    Rack::Attack::Allow2Ban.filter(req.ip, :maxretry => 20, :findtime => 1.minute, :bantime => 1.hour) do
-      # The count for the IP is incremented if the return value is truthy.
-      req.path == '/login' and req.post?
-    end
-  end
-
-  #For Admin, blocks ip address
-  Rack::Attack.blocklist('allow2ban login scrapers') do |req|
-    # `filter` returns false value if request is to your login page (but still
-    # increments the count) so request below the limit are not blocked until
-    # they hit the limit.  At that point, filter will return true and block.
-    Rack::Attack::Allow2Ban.filter(req.ip, :maxretry => 10, :findtime => 1.minute, :bantime => 1.hour) do
-      # The count for the IP is incremented if the return value is truthy.
-      req.path == '/login' and req.post?
-    end
-  end
-  
 end
+
+
+
+# # Lockout IP addresses that are hammering your login page.
+# # After 20 requests in 1 minute, block all requests from that IP for 1 hour.
+# Rack::Attack.blocklist('allow2ban login scrapers') do |req|
+#   # `filter` returns false value if request is to your login page (but still
+#   # increments the count) so request below the limit are not blocked until
+#   # they hit the limit.  At that point, filter will return true and block.
+#   Rack::Attack::Allow2Ban.filter(req.ip, :maxretry => 20, :findtime => 1.minute, :bantime => 1.hour) do
+#     # The count for the IP is incremented if the return value is truthy.
+#     req.path == '/login' and req.post?
+#   end
+# end
+#
+# #For Admin, blocks ip address
+# Rack::Attack.blocklist('allow2ban login scrapers') do |req|
+#   # `filter` returns false value if request is to your login page (but still
+#   # increments the count) so request below the limit are not blocked until
+#   # they hit the limit.  At that point, filter will return true and block.
+#   Rack::Attack::Allow2Ban.filter(req.ip, :maxretry => 10, :findtime => 1.minute, :bantime => 1.hour) do
+#     # The count for the IP is incremented if the return value is truthy.
+#     req.path == '/login' and req.post?
+#   end
+# end
