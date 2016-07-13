a5cf4cad3bcde38b0ecf604adae45a4994a34d3b
Lyndon McKay <lyndonmckay13@gmail.com>
2016-07-12 22:18:50 -0500

    Add Image to API

--- app/api/entity/v1/basic_food_items_entity.rb
+++ app/api/entity/v1/basic_food_items_entity.rb
@@ -1,7 +1,8 @@
 module Entity
   module V1
     class BasicFoodItemsEntity < Grape::Entity
-      expose :id, :title, :updated_at, :is_basic, :fridge_amount, :shoppinglist_amount, :mylist_amount, :measurement_type, :full_amount, :current_amount, :usually_expires, :expiration_date, :modified_date, :categories
+      expose :id, :title, :updated_at, :is_basic, :fridge_amount, :shoppinglist_amount, :mylist_amount, :measurement_type,
+      :full_amount, :current_amount, :usually_expires, :expiration_date, :modified_date, :categories, :image
     end
   end
 end
