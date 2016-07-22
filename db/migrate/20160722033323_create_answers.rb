526c86825acc376d4228aeac40bbf6b9afbb16c5
Lyndon McKay <lyndonmckay13@gmail.com>
2016-07-21 22:54:47 -0500

    Changed to integer

--- db/migrate/20160722033323_create_answers.rb
+++ db/migrate/20160722033323_create_answers.rb
@@ -2,7 +2,7 @@ class CreateAnswers < ActiveRecord::Migration
   def change
     create_table :answers do |t|
       t.string :answer
-      t.Int :question_id
+      t.integer :question_id
       t.string :question
 
       t.timestamps null: false
