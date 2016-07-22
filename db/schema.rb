823cb4603191c597a0395ccd8aa235da88941130
Lyndon McKay <lyndonmckay13@gmail.com>
2016-07-22 00:40:11 -0500

    Renamed survey.type to stype

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160722030756) do
+ActiveRecord::Schema.define(version: 20160722053316) do
 
   create_table "admins", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -33,6 +33,14 @@ ActiveRecord::Schema.define(version: 20160722030756) do
 
   add_index "admins", ["email"], name: "index_admins_on_email", unique: true
   add_index "admins", ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+
+  create_table "answers", force: :cascade do |t|
+    t.string   "answer"
+    t.integer  "question_id"
+    t.string   "question"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "basic_food_items", force: :cascade do |t|
     t.string   "title"
@@ -143,6 +151,8 @@ ActiveRecord::Schema.define(version: 20160722030756) do
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
+    t.string   "stype"
+    t.boolean  "pre_question"
   end
 
   create_table "users", force: :cascade do |t|
