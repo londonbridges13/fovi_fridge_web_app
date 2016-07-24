f5cb03c79207b623a7c29e8b35dbd52addf75c40
Lyndon McKay <lyndonmckay13@gmail.com>
2016-07-24 14:41:44 -0500

    Added Options to AAPI

--- app/api/entity/v1/surveys_entity.rb
+++ app/api/entity/v1/surveys_entity.rb
@@ -1,7 +1,8 @@
 module Entity
   module V1
     class SurveysEntity < Grape::Entity
-      expose :id, :question, :image, :pre_question, :stype
+      expose :id, :question, :image, :pre_question, :stype, :option1, :option2, :option3,
+       :option4, :option5
     end
   end
 end
