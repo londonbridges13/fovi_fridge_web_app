d459077b9b4ae6b56ab7a68fef44f6655897af9f
Lyndon McKay <lyndonmckay13@gmail.com>
2016-08-02 10:47:47 -0500

    Added       :fridge_usually_expires, :

--- app/api/entity/v1/basic_food_items_entity.rb
+++ app/api/entity/v1/basic_food_items_entity.rb
@@ -3,7 +3,7 @@ module Entity
     class BasicFoodItemsEntity < Grape::Entity
       expose :id, :title, :updated_at, :is_basic, :fridge_amount, :shoppinglist_amount, :mylist_amount, :measurement_type,
       :full_amount, :current_amount, :usually_expires, :expiration_date, :modified_date, :categories, :image, :image_url,
-      :fridge_usually_expires
+      :fridge_usually_expires, :food_category
     end
   end
 end
