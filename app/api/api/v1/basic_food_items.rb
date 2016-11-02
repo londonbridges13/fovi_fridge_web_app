7a16006b5fcc9cb5d37e31bdbf90cb15e6277f5b
Lyndon McKay <lyndonmckay13@gmail.com>
2016-11-01 19:08:51 -0500

    removed bfi/get api

--- app/api/api/v1/basic_food_items.rb
+++ app/api/api/v1/basic_food_items.rb
@@ -16,20 +16,21 @@ module API
 
         desc 'API For Collecting New BasicFoodItems'
         post do
-          @basic_food_item = BasicFoodItem.create!({
-            title: params[:title], measurement_type: params[:measurement_type],
-              calories: params[:calories], total_fat: params[:total_fat],
-              saturated_fat: params[:saturated_fat], unsaturated_fat: params[:unsaturated_fat], protien: params[:protien],
-               carbohydrate: params[:carbohydrate], fiber: params[:fiber], food_category: params[:food_category],
-               iron: params[:iron], vitamin_a: params[:vitamin_a], vitamin_b12: params[:vitamin_b12], vitamin_b6: params[:vitamin_b6],
-               vitamin_c: params[:vitamin_c], vitamin_d: params[:vitamin_d],  vitamin_e: params[:vitamin_e],
-               vitamin_k: params[:vitamin_k], potassium: params[:potassium], sodium: params[:sodium], niacin: params[:niacin],
-                folate: params[:folate], zinc: params[:zinc], thiamin: params[:thiamin], riboflavin: params[:riboflavin],
-                 calcium: params[:calcium], magnesium: params[:magnesium], cholesterol: params[:cholesterol], caffeine: params[:caffeine]
-            })
-            @basic_food_item.is_basic = true
-            @basic_food_item.is_new = true
-            @basic_food_item.save!
+          # @basic_food_item = BasicFoodItem.create!({
+          #   title: params[:title], measurement_type: params[:measurement_type],
+          #     calories: params[:calories], total_fat: params[:total_fat],
+          #     saturated_fat: params[:saturated_fat], unsaturated_fat: params[:unsaturated_fat], protien: params[:protien],
+          #      carbohydrate: params[:carbohydrate], fiber: params[:fiber],
+          #      iron: params[:iron], vitamin_a: params[:vitamin_a], vitamin_b12: params[:vitamin_b12], vitamin_b6: params[:vitamin_b6],
+          #      vitamin_c: params[:vitamin_c], vitamin_d: params[:vitamin_d],  vitamin_e: params[:vitamin_e],
+          #      vitamin_k: params[:vitamin_k], potassium: params[:potassium], sodium: params[:sodium], niacin: params[:niacin],
+          #       folate: params[:folate], zinc: params[:zinc], thiamin: params[:thiamin], riboflavin: params[:riboflavin],
+          #        calcium: params[:calcium], magnesium: params[:magnesium], cholesterol: params[:cholesterol], caffeine: params[:caffeine]
+          #   })
+          #   @basic_food_item.is_basic = true
+          #   @basic_food_item.food_category = "Others" #default
+          #   @basic_food_item.is_new = true
+          #   @basic_food_item.save!
         end
       end
     end
